0c84d7dea597d0bb3182f503a2cacc67387d2e5c
Sally Jean Sumeguin <sumeguinsallyjean02@gmail.com>
2024-04-18 10:32:10 +0800

    login

--- resources/js/Components/Main.tsx
+++ resources/js/Components/Main.tsx
@@ -1,9 +1,43 @@
-import React from "react"
+import React from "react";
+import {
+    Routes,
+    Route,
+    Link,
+    HashRouter as Router
+} from "react-router-dom";
+import { Login } from "./Login";
 
 export default function Main() {
     return (
-        <div>
-            <h1>Main</h1>
-        </div>
+        <Router>
+            <div>
+                {/* <nav>
+                    <ul>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="/job-seeker">Job Seeker</Link>
+                        </li>
+                        <li>
+                            <Link to="/job-employer">Job Emplooyer</Link>
+                        </li>
+                        <li>
+                            <Link to="/admin">Admin</Link>
+                        </li>
+                    </ul>
+                </nav> */}
+
+                <Routes>
+                    <Route path="/job-seeker">
+                        {/* <About /> */}
+                    </Route>
+                    <Route path="/job-employeer">
+                        {/* <Users /> */}
+                    </Route>
+                    <Route path="/login" Component={Login}></Route>
+                </Routes>
+            </div>
+        </Router>
     )
 }
